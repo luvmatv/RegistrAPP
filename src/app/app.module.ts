850f741147c49502d965aedc0c548b8c8ff342d6
luvmatv <luk.rojasm@duocuc.cl>
2024-11-12 21:41:49 -0300

    horarios-cargados
    Se implementa un horario específico para cada carrera, el cual sigue el formato solicitado. Además, cuando un ramo se encuentre dentro del tiempo correspondiente, será marcado en verde, lo que indica que está en transcurso y es utilizable.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';  
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';  
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +23,10 @@ import { AppRoutingModule } from './app-routing.module';
     IonicStorageModule.forRoot(),
     HttpClientModule  
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }, 
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
