9ace7305bb38739f7eeda69fa5c3729180560289
luvmatv <luk.rojasm@duocuc.cl>
2024-11-14 20:27:56 -0300

    home-carga
    implementación de pantalla de carga minimalista en el page "home", eliminando botones innecesarios e información sin ningún aporte.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-import { NavController, AnimationController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,53 +8,13 @@ import { NavController, AnimationController } from '@ionic/angular';
 })
 export class HomePage implements AfterViewInit {
 
-  @ViewChild('title', { read: ElementRef }) title!: ElementRef;
-  @ViewChild('welcomeText', { read: ElementRef }) welcomeText!: ElementRef;
-  @ViewChild('description', { read: ElementRef }) description!: ElementRef;
-  @ViewChild('loginButton', { read: ElementRef }) loginButton!: ElementRef;
+  @ViewChild('loadingAnimation', { static: false }) loadingAnimation!: ElementRef;
 
-  constructor(private navCtrl: NavController, private animationCtrl: AnimationController) {}
+  constructor(private navCtrl: NavController) {}
 
   ngAfterViewInit() {
-    console.log("ngAfterViewInit called");  
-    this.animateElements();
-  }
-
-  animateElements() {
-    const titleAnimation = this.animationCtrl.create()
-      .addElement(this.title.nativeElement)
-      .duration(1000)
-      .fromTo('transform', 'translateY(-100px)', 'translateY(0)')
-      .fromTo('opacity', '0', '1');
-
-    const welcomeAnimation = this.animationCtrl.create()
-      .addElement(this.welcomeText.nativeElement)
-      .duration(1000)
-      .fromTo('opacity', '0', '1')
-      .fromTo('transform', 'translateX(-50px)', 'translateX(0)')
-      .delay(500);  
-    
-    const descriptionAnimation = this.animationCtrl.create()
-      .addElement(this.description.nativeElement)
-      .duration(1000)
-      .fromTo('opacity', '0', '1')
-      .fromTo('transform', 'translateX(50px)', 'translateX(0)')
-      .delay(1000);  
-
-    const buttonAnimation = this.animationCtrl.create()
-      .addElement(this.loginButton.nativeElement)
-      .duration(1000)
-      .fromTo('opacity', '0', '1')
-      .fromTo('transform', 'translateY(100px)', 'translateY(0)')
-      .delay(1500);  
-
-    titleAnimation.play();
-    welcomeAnimation.play();
-    descriptionAnimation.play();
-    buttonAnimation.play();
-  }
-
-  goToLogin() {
-    this.navCtrl.navigateForward('/login');
+    setTimeout(() => {
+      this.navCtrl.navigateForward('/login');
+    }, 3000);  
   }
 }
